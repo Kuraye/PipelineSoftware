87d79ec1467ae35deda6a0efc224f62effc97ae4
Tony Nguyen <113423512+Kuraye@users.noreply.github.com>
2024-11-08 11:32:18 +0100

    fix error

--- test.js
+++ test.js
@@ -1,36 +1,31 @@
-Fconst pdfParse = require('pdf-parse');
+const pdfParse = require('pdf-parse');
 const fs = require('fs');
 
 describe('PDF Content Tests', () => {
-  it('should check if the PDF file exists', async () => {
+  it('should check if the PDF file exists and its content', async () => {
     const pdfPath = 'PolicyDocument.pdf';
 
     if (fs.existsSync(pdfPath)) {
-      console.log('5.1.A. Policy document exists');
+      const pdfData = await pdfParse(pdfPath);
+      const text = pdfData.text.toLowerCase();
+
+      const reportFile = 'test_report.txt';
+      fs.writeFileSync(reportFile, "# This document has the results if the application complies with the ISO 27001 and NEN 7510\n");
+      fs.appendFileSync(reportFile, "5.1.A. Policy document exists\n");
+
+      if (text.includes('organization specific')) {
+        fs.appendFileSync(reportFile, "5.2.A. Policy document is tailored\n");
+      } else {
+        fs.appendFileSync(reportFile, "[!] 5.2.A. Policy document is not tailored\n");
+      }
+
+      if (text.includes('commitment to compliance')) {
+        fs.appendFileSync(reportFile, "5.2.C. Policy document contains Commitment to compliance\n");
+      } else {
+        fs.appendFileSync(reportFile, "[!] 5.2.C. Policy document does not contain Commitment to compliance\n");
+      }
     } else {
-      console.log('[!] 5.1.A. Policy document does not exist');
-    }
-  });
-
-  it('should check for specific strings in the PDF', async () => {
-    const pdfPath = 'PolicyDocument.pdf';
-
-    const pdfData = await pdfParse(pdfPath);
-    const text = pdfData.text.toLowerCase();
-
-    const reportFile = 'test_report.txt';
-    fs.writeFileSync(reportFile, 'Test Report:\n');
-
-    if (text.includes('organization specific')) {
-      fs.appendFileSync(reportFile, '5.2.A. Policy document is tailored\n');
-    } else {
-      fs.appendFileSync(reportFile, '[!] 5.2.A. Policy document is not tailored\n');
-    }
-
-    if (text.includes('commitment to compliance')) {
-      fs.appendFileSync(reportFile, '5.2.C. Policy document contains commitment to compliance\n');
-    } else {
-      fs.appendFileSync(reportFile, '[!] 2.2.C. Policy document does not contain commitment to compliance\n');
+      fs.writeFileSync('test_report.txt', "[!] 5.1.A. Policy document missing\n");
     }
   });
 });
