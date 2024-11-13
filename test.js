7ac94644b6060cebaeb264c6f4d58c790945a50d
Tony Nguyen <113423512+Kuraye@users.noreply.github.com>
2024-11-13 14:12:15 +0100

    add 5.2.B

--- test.js
+++ test.js
@@ -21,6 +21,12 @@ describe('PDF Content Tests', () => {
         fs.appendFileSync(reportFile, "[!] 5.2.A. Policy document is not tailored\n");
         nonComplianceList.push('5.2.A');
       }
+      if (text.includes('security objectives')) {
+        fs.appendFileSync(reportFile, "5.2.B. Security objectives documented\n");
+      } else {
+        fs.appendFileSync(reportFile, "[!] 5.2.B. Security objectives not documented\n");
+        nonComplianceList.push('5.2.B');
+      }
 
       if (text.includes('commitment to compliance')) {
         fs.appendFileSync(reportFile, "5.2.C. Policy document contains Commitment to compliance\n");
