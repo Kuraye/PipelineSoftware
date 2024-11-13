170eb846c64d4b31f05d80c86182b5065586910e
Tony Nguyen <113423512+Kuraye@users.noreply.github.com>
2024-11-13 13:45:30 +0100

    change spacing

--- test.js
+++ test.js
@@ -13,23 +13,23 @@ describe('PDF Content Tests', () => {
 
       const reportFile = 'test_report.txt';
       fs.writeFileSync(reportFile, "# This document has the results if the application complies with the ISO 27001 and NEN 7510\n");
-      fs.appendFileSync(reportFile, "5.1.A. Policy document exists\n");
+      fs.appendFileSync(reportFile, "    5.1.A. Policy document exists\n");
 
       if (text.includes('organization specific')) {
-        fs.appendFileSync(reportFile, "5.2.A. Policy document is tailored\n");
+        fs.appendFileSync(reportFile, "    5.2.A. Policy document is tailored\n");
       } else {
         fs.appendFileSync(reportFile, "[!] 5.2.A. Policy document is not tailored\n");
         nonComplianceCount++;
       }
       if (text.includes('security objectives')) {
-        fs.appendFileSync(reportFile, "5.2.B. Security objectives documented\n");
+        fs.appendFileSync(reportFile, "    5.2.B. Security objectives documented\n");
       } else {
         fs.appendFileSync(reportFile, "[!] 5.2.B. Security objectives not documented\n");
         nonComplianceCount++;
       }
 
       if (text.includes('commitment to compliance')) {
-        fs.appendFileSync(reportFile, "5.2.C. Policy document contains Commitment to compliance\n");
+        fs.appendFileSync(reportFile, "    5.2.C. Policy document contains Commitment to compliance\n");
       } else {
         fs.appendFileSync(reportFile, "[!] 5.2.C. Policy document does not contain Commitment to compliance\n");
         nonComplianceCount++;
