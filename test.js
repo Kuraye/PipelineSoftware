f992d071da5ae031b47066115663222215844b0b
Tony Nguyen <113423512+Kuraye@users.noreply.github.com>
2024-11-13 14:10:04 +0100

    change non compliance

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const pdfParse = require('pdf-parse');
 const fs = require('fs');
 
 describe('PDF Content Tests', () => {
-  let nonComplianceCount = 0;
+  const nonComplianceList = [];
 
   it('should check if the PDF file exists and its content', async () => {
     const pdfPath = 'PolicyDocument.pdf';
@@ -13,34 +13,33 @@ describe('PDF Content Tests', () => {
 
       const reportFile = 'test_report.txt';
       fs.writeFileSync(reportFile, "# This document has the results if the application complies with the ISO 27001 and NEN 7510\n");
-      fs.appendFileSync(reportFile, "    5.1.A. Policy document exists\n");
+      fs.appendFileSync(reportFile, "5.1.A. Policy document exists\n");
 
       if (text.includes('organization specific')) {
-        fs.appendFileSync(reportFile, "    5.2.A. Policy document is tailored\n");
+        fs.appendFileSync(reportFile, "5.2.A. Policy document is tailored\n");
       } else {
         fs.appendFileSync(reportFile, "[!] 5.2.A. Policy document is not tailored\n");
-        nonComplianceCount++;
-      }
-      if (text.includes('security objectives')) {
-        fs.appendFileSync(reportFile, "    5.2.B. Security objectives documented\n");
-      } else {
-        fs.appendFileSync(reportFile, "[!] 5.2.B. Security objectives not documented\n");
-        nonComplianceCount++;
+        nonComplianceList.push('5.2.A');
       }
 
       if (text.includes('commitment to compliance')) {
-        fs.appendFileSync(reportFile, "    5.2.C. Policy document contains Commitment to compliance\n");
+        fs.appendFileSync(reportFile, "5.2.C. Policy document contains Commitment to compliance\n");
       } else {
         fs.appendFileSync(reportFile, "[!] 5.2.C. Policy document does not contain Commitment to compliance\n");
-        nonComplianceCount++;
+        nonComplianceList.push('5.2.C');
       }
+
+      fs.appendFileSync(reportFile, `\nNon-Compliance List:\n`);
+      nonComplianceList.forEach(item => {
+        fs.appendFileSync(reportFile, `- ${item}\n`);
+      });
     } else {
       fs.writeFileSync('test_report.txt', "[!] 5.1.A. Policy document missing\n");
-      nonComplianceCount++;
+      nonComplianceList.push('5.1.A');
+      fs.appendFileSync(reportFile, `\nNon-Compliance List:\n`);
+      nonComplianceList.forEach(item => {
+        fs.appendFileSync(reportFile, `- ${item}\n`);
+      });
     }
-  });
-
-  afterAll(() => {
-    fs.appendFileSync('test_report.txt', `\n\nTotal Non-Compliance Issues: ${nonComplianceCount}`);
   });
 });
