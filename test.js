f2cf72be00977effe760bddde27d48b6e40ebb26
Tony Nguyen <113423512+Kuraye@users.noreply.github.com>
2024-11-19 15:51:33 +0100

    update spacing

--- test.js
+++ test.js
@@ -13,24 +13,24 @@ describe('PDF Content Tests', () => {
     if (fs.existsSync(pdfPath)) {
       const pdfData = await pdfParse(pdfPath);
       const text = pdfData.text.toLowerCase();
-      fs.writeFileSync(reportFile, "5.1.A. Policy document exists\n");
+      fs.writeFileSync(reportFile, "    5.1.A. Policy document exists\n");
 
       if (text.includes('organization specific')) {
-        fs.appendFileSync(reportFile, "  5.2.A. Policy document is tailored\n");
+        fs.appendFileSync(reportFile, "    5.2.A. Policy document is tailored\n");
       } else {
         fs.appendFileSync(reportFile, "[!] 5.2.A. Policy document is not tailored\n");
         nonComplianceList.push('5.2.A');
       }
 
       if (text.includes('security objectives')) {
-        fs.appendFileSync(reportFile, "  5.2.B. Security Objectives documented\n");
+        fs.appendFileSync(reportFile, "    5.2.B. Security Objectives documented\n");
       } else {
         fs.appendFileSync(reportFile, "[!] 5.2.B. Security Objectives not documented\n");
         nonComplianceList.push('5.2.B');
       }
 
       if (text.includes('commitment to compliance')) {
-        fs.appendFileSync(reportFile, "  5.2.C. Policy document contains Commitment to compliance\n");
+        fs.appendFileSync(reportFile, "    5.2.C. Policy document contains Commitment to compliance\n");
       } else {
         fs.appendFileSync(reportFile, "[!] 5.2.C. Policy document does not contain Commitment to compliance\n");
         nonComplianceList.push('5.2.C');
@@ -57,10 +57,10 @@ describe('PDF Content Tests', () => {
 
     try {
       const riskTreatmentPlanData = await riskTreatmentPlanPromise;
-      fs.appendFileSync(reportFile, "  8.1.A. Risk Treatment Plan exists\n");
+      fs.appendFileSync(reportFile, "    8.1.A. Risk Treatment Plan exists\n");
 
       if (riskTreatmentPlanData.includes('treatment plan details')) {
-        fs.appendFileSync(reportFile, "  8.1.B. Risk Treatment Plan contains treatment plan details\n");
+        fs.appendFileSync(reportFile, "    8.1.B. Risk Treatment Plan contains treatment plan details\n");
       } else {
         fs.appendFileSync(reportFile, "[!] 8.1.B. Risk Treatment Plan does not contain treatment plan details\n");
         nonComplianceList.push('8.1.B');
