8a2abeb0df9707788d8139a44daad56ed6de83c0
Tony Nguyen <113423512+Kuraye@users.noreply.github.com>
2024-11-25 11:18:57 +0100

    change logic in structure of test 8.1.b

--- test.js
+++ test.js
@@ -56,24 +56,25 @@ describe('PDF Content Tests', () => {
       }
     });
 
-    if (fs.existsSync(riskTreatmentPlanPath)) {
-      fs.appendFileSync(reportFile, "8.1.A. Risk Treatment Plan exists\n");
+if (fs.existsSync(riskTreatmentPlanPath)) {
+  fs.appendFileSync(reportFile, "8.1.A. Risk Treatment Plan exists\n");
 
-      const results = [];
-      fs.createReadStream(riskTreatmentPlanPath)
-        .pipe(csvParser()) // Use csvParser() to parse the CSV data
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-          if (!results.some(row => row['Risk ID'] && row['Treatment'] && row['Owner'])) {
-            fs.appendFileSync(reportFile, "[!] 8.1.B. Risk Treatment Plan is missing required columns\n");
-            nonComplianceList.push('8.1.B. Risk Treatment Plan is missing required columns');
-          } else {
-          }
-        });
-    } else {
-      fs.appendFileSync(reportFile, "[!] 8.1.A. Risk Treatment Plan missing\n");
-      nonComplianceList.push('8.1.A. Risk Treatment Plan missing');
-    }
+  const results = [];
+  fs.createReadStream(riskTreatmentPlanPath)
+    .pipe(csvParser())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+      if (!results.some(row => row['Risk ID'] && row['Treatment'] && row['Owner'])) {
+        fs.appendFileSync(reportFile, "[!] 8.1.B. Risk Treatment Plan is missing required columns\n");
+        nonComplianceList.push('8.1.B. Risk Treatment Plan is missing required columns');
+      } else {
+
+      }
+    });
+} else {
+  fs.appendFileSync(reportFile, "[!] 8.1.A. Risk Treatment Plan missing\n");
+  nonComplianceList.push('8.1.A. Risk Treatment Plan missing');
+}
 
     fs.appendFileSync(reportFile, `\nNon-Compliance List:\n`);
     nonComplianceList.forEach(item => {
